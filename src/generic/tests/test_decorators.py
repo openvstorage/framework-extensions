14b129b719253c187201a6a95b62488f3ae2536d
Simon Van den Bossche <simon.vandenbossche@openvstorage.com>
2018-01-18 10:26:22 +0100

    fixed decorators test

--- src/generic/tests/test_decorators.py
+++ src/generic/tests/test_decorators.py
@@ -17,6 +17,7 @@
 OVS-extensionds decorator test class
 """
 
+import os
 import time
 import unittest
 from ovs_extensions.generic.decorators import timeout, TimeoutError
@@ -29,6 +30,26 @@ class TestDecorators(unittest.TestCase):
         self.assertTrue(TimeoutError, self._count_to(n=2))
 
     @timeout(3)
-    def _count_to(self, n=5):
-        time.sleep(n)
-        return True
+    def _count_to(self,  n=5):
+        file_path = None
+        file_name = 'ovs_extentions_test_decorators.txt'
+        try:
+            with open(file_name, 'w') as fh:
+                file_path = os.path.realpath(fh.name)
+                for i in range(n):
+                    fh.write('{0}\n'.format(i))
+                    time.sleep(1)
+                return TestDecorators._validate_contents(file_name, n)
+        finally:
+            if file_path is not None:
+                #os.remove(file_path)
+                pass
+    @staticmethod
+    def _validate_contents(file_name, count):
+        with open(file_name, 'r') as fh:
+            if fh.read() == '\n'.join([str(i) for i in range(count)]).strip():
+                return True
+            else:
+                return False
+
+
