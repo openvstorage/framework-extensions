0ae91adcc0747d8679253dd4fb5096ee477a1849
Jeffrey Devloo <jeffreydevloo@hotmail.com>
2018-06-19 10:38:56 +0200

    SystemdMock now creates new PIDs on process restart Added exceptions module for tests

---
+++ src/testing/exceptions.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2018 iNuron NV
+#
+# This file is part of Open vStorage Open Source Edition (OSE),
+# as available from
+#
+#      http://www.openvstorage.org and
+#      http://www.openvstorage.com.
+#
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License v3 (GNU AGPLv3)
+# as published by the Free Software Foundation, in version 3 as it comes
+# in the LICENSE.txt file of the Open vStorage OSE distribution.
+#
+# Open vStorage is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY of any kind.
+
+"""
+Testing exception module
+Contains exception used for unit testing
+"""
+
+
+class WorkerLossException(EOFError):
+    """
+    Thrown when the current worker would be killed
+    """
+    pass
