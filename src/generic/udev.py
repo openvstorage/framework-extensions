394465f3ea48f3d87a809ed1663aa3c019aed906
Simon Van den Bossche <simon.vandenbossche@openvstorage.com>
2018-08-24 10:37:04 +0200

    Fixed remarks + additional docstrings

--- src/generic/udev.py
+++ src/generic/udev.py
@@ -26,44 +26,64 @@ class UDevGenerator(object):
     base_path = '/lib/udev/rules.d/{0}'
 
     def __init__(self, name, level=99):
-        # type(str, Optional[int]) -> None
+        # type: (str, Optional[int]) -> None
         """
         Generates a Udev generator object, that can be expanded with various seperate rules.
-        :param name: Name that will be
-        :param level:
+        :param name: Name that will be used for the udev rule.
+        :param level: Level of order assigned to the udev rule. Rule 1 will be assigned first, 99 last.
         """
         self.name = name
         self.level = level
         self.content = []
 
     def add_rule(self, rule):
-        # type(UDevRule) -> None
+        # type: (UDevRule) -> None
+        """
+        Add a UDevRule to the content of this object.
+        :param rule: UDevRule object
+        :return: None
+        """
         if not isinstance(rule, UDevRule):
             raise RuntimeError('Illegal argument: rule must be of type {0}'.format(UDevRule))
         self.content.append(rule)
 
     def make_name(self):
+        # type: (None) -> str
         return '{0}-{1}.rule'.format(self.level, self.name)
 
     def write_content(self, path=base_path):
+        # type: (Optional[str]) -> None
+        """
+        Will write the content attribute of the UDevGenerator object to the udev rule file with UDevGenerator.make_name
+        :param path: the base path of the udev rule folder
+        :return: None
+        """
         path = path.format(self.make_name())
-        with open(path) as fh:
+        with open(path, 'w') as fh:
             fh.write(str(self))
 
     def __str__(self):
         return '\n'.join([str(i) for i in self.content])  # Stringify every rule instance
 
 
-class UDevRule():
-    operatordict = OrderedDict(
-        [('bus', '=='),
+class UDevRule(object):
+    operatordict = [('bus', '=='),
          ('kernel', '=='),
          ('subsystem', '=='),
          ('name', '='),
          ('symlink', '+=')]
-    )
 
     def __init__(self, kernel, name, bus=None, subsystem=None, symlink=None):
+        # type: (str, str, Optional[str], Optional[str], Optional[str]) -> None
+        """
+        The parameters of the UDevRule object reflect the fields that are allowed in udev rules.
+        str(UDevRule) will return a string with a correctly formatted udev rule.
+        :param kernel: Kernel of the device of interest
+        :param name: Name that will be given to the device
+        :param bus: Bus of the device of interest
+        :param subsystem: Subsystem of the device of interest
+        :param symlink: Symlink that will be given to the device
+        """
         self.bus = bus
         self.kernel = kernel
         self.name = name
@@ -72,8 +92,8 @@ class UDevRule():
 
     def __str__(self):
         out = []
-        for key, value in self.operatordict.items():
+        for key, value in self.operatordict:
             attribute_value = getattr(self, key)
             if attribute_value:
-                out.append('{0}{1}"{2}"'.format(key.upper(), self.operatordict.get(key), attribute_value))
+                out.append('{0}{1}"{2}"'.format(key.upper(), value, attribute_value))
         return ', '.join(out)
