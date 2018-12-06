e54060034675119311c106020fb24bf2929e317a
Simon Van den Bossche <simon.vandenbossche@openvstorage.com>
2018-12-06 17:08:15 +0100

    changed some constants paths, added migration code todo: test migration code

--- src/constants/framework.py
+++ src/constants/framework.py
@@ -22,8 +22,9 @@ import os
 FRAMEWORK_BASE = '/ovs/framework/'
 
 ### Remote config
-REMOTE_CONFIG_BACKEND = 'ovs/framework/remote_configs/alba_backends/{0}/abm_config'
-REMOTE_CONFIG_BACKEND_INI = 'ovs/framework/remote_configs/alba_backends/{0}/abm_config/ini'
+REMOTE_CONFIG_BACKENDS_BASE = 'ovs/framework/remote_configs/alba_backends/'
+REMOTE_CONFIG_BACKEND_CONFIG = os.path.join(REMOTE_CONFIG_BACKENDS_BASE, '{0}/abm_config')
+REMOTE_CONFIG_BACKEND_INI = os.path.join(REMOTE_CONFIG_BACKEND_CONFIG, 'ini')  # ovs/framework/remote_configs/alba_backends/{abe_guid}/abm_config/ini
 
 ### NBD related config paths
 NBD = os.path.join(FRAMEWORK_BASE, 'nbdnodes')
